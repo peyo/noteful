8398de9ce64148f374e56a81c538d46241428568
peyo <peteryyoon@gmail.com>
2020-04-23 13:53:42 -0700

    updated for zeit

---
+++ build/precache-manifest.2d8c3cc85859abd65ea8e3e006d3cb58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f39e1c8be6cce90ce57416decb975d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52665f0f8f910a287671",
+    "url": "/static/css/main.9fa26fa5.chunk.css"
+  },
+  {
+    "revision": "5a622c1f7e121302aba7",
+    "url": "/static/js/2.9ce40288.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.9ce40288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52665f0f8f910a287671",
+    "url": "/static/js/main.258d050a.chunk.js"
+  },
+  {
+    "revision": "5e2daf7e8d7fbea81ca1",
+    "url": "/static/js/runtime-main.ef885c1d.js"
+  }
+]);
